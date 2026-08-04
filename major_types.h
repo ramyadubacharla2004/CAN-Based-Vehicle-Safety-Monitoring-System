2d0ba225bb02b113b8670a46cadf5e60ee82e9c9
ramyadubacharla2004 <ramyadubacharla2004@gmail.com>
2026-08-04 19:45:57 +0530

    Create major_types.h

---
+++ major_types.h
@@ -0,0 +1,9 @@
+//major_type.h
+typedef unsigned char u8;
+typedef char s8;
+typedef unsigned int u32;
+typedef int s32;       
+typedef unsigned short int u16;
+typedef signed short int s16;
+typedef float f32;
+typedef double d64;
