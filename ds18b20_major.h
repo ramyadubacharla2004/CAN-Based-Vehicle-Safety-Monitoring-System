26c6d72ae4e50f48b6b1e201f7a6807a4c7bac51
ramyadubacharla2004 <ramyadubacharla2004@gmail.com>
2026-08-04 19:48:23 +0530

    Create ds18b20_major.h

---
+++ ds18b20_major.h
@@ -0,0 +1,13 @@
+//ds18b20_major.h
+#ifndef _DS18B20_H_
+#define _DS18B20_H_
+
+/**DS18B20 FUNCTION DECLARATION**/
+unsigned char ResetDS18b20(void);
+unsigned char ReadBit(void);
+void WriteBit(unsigned char);
+unsigned char ReadByte(void);
+void WriteByte(unsigned char);
+int ReadTemp(void);
+
+#endif
